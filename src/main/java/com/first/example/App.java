8369f394cb299c1c8fb5814ff4575bd75c6c4d78
mitali2507 <mit.angeleyes@gmail.com>
2015-08-04 23:18:21 +0530

    my first commit

---
+++ src/main/java/com/first/example/App.java
@@ -0,0 +1,14 @@
+package com.first.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "Spring-Module.xml");
+
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        obj.printHello();
+    }
+}
